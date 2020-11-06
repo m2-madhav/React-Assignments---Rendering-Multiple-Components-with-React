5d72b648b50055ea2e2ff7d848291341cfd7f2ef
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-06 16:16:32 +0530

    code-added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name"> What's Your Location</h1>
+      <p data-ns-test="project-description">
+        {" "}
+        A login page is there and in this different location's of friends is
+        there and through emailId we can sign up this and get into this group
+      </p>
+      <h1 data-ns-test="project-name"> Thanks Doctor</h1>
+      <p data-ns-test="project-description">
+        {" "}
+        In this different names of doctor,Hospital and chemist is present and we
+        can get connected to them{" "}
+      </p>
+    </>
+  );
+};
 
 export default App;
